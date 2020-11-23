e6a7b3525c3b76901e73237050be20ced5d024ec
hudzzz <muhammadhudzaifahassyahid5@gmail.com>
2020-11-23 21:43:03 +0700

    anuan

--- app/src/main/java/main/jnakshansh/bottomnavigationbar/MainActivity.java
+++ app/src/main/java/main/jnakshansh/bottomnavigationbar/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     // KITA
-    
+    // anuan
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
